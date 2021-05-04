6da3df2fa9e5c5cd015f72f36ada338b99fe3c9b
Pugovkina <tatiana_pugovkin@mail.ru>
2021-05-04 15:48:17 +0600

    Добавление библиотеки рисования солнца и облака

--- Multfilm_biblioteka.cpp
+++ Multfilm_biblioteka.cpp
@@ -9,16 +9,26 @@
 #include <TXLib.h>
 #include "Pugovkina.h"
 
-void Fon_osnova ();
+void Morning ();
 
 int main()
 	{
-	Fon_osnova ();
+	txCreateWindow (1100, 800);
+	
+	txBegin ();
+	
+	Titles (RGB (89, 184, 33), TX_WHITE, "Мультфильм", "Прогулка c подружками");
+	
+	Morning ();
+	
+	Titles (RGB (89, 184, 33), TX_WHITE, "Автор сценария", "Пуговкина Татьяна");
+	
+	txEnd ();
 	
 	return 0;	
 	}
 
-void Fon_osnova ()
+void Morning ()
 	{
 	Fon_Rectangle (RGB (151, 255, 255), TX_LIGHTGREEN);
 	
@@ -26,5 +36,14 @@ void Fon_osnova ()
 		Flowers (rand() % 1100, 260 + rand() % 800, 
 				RGB ( 60 + rand() % 80, 190 + rand() % 30, 200 + rand() %  40), 
 				RGB (210 + rand() % 11, 210 + rand() % 11,   0 + rand() % 166));
-	}
+	    
+	for (int kolichestvo = 0; kolichestvo <= 7; kolichestvo ++)	
+		CloudDraw (200 + rand() % 1000, 230 - rand() % 450, 1, 1, RGB (160, 217, 250));
+	
+	SolnceDraw (150, 100, 1, 1, -2.5, 1, 1, TX_YELLOW);
+			
+	txSleep (1000);
+    }
+  
+
 
